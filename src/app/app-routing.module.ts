5bc1cbeef81d40fbfa987bd5706336bcc6b55f77
Varadi-Marcell <marcibazsi11@gmail.com>
2023-06-20 17:59:06 +0200

    delete commented lines

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {inject, NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TodoListComponent} from "./todo-list/todo-list.component";
+import {TodoService} from "./service/todo.service";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoListComponent,
+    resolve: {
+      init: () => inject(TodoService).init()
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TodoListComponent} from './todo-list/todo-list.component';
+import {CreateTodoComponent} from './create-todo/create-todo.component';
+import {UpdateTodoComponent} from './update-todo/update-todo.component';
+import {ViewTodoComponent} from './view-todo/view-todo.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BsModalService, ModalModule} from "ngx-bootstrap/modal";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    CreateTodoComponent,
+    UpdateTodoComponent,
+    ViewTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ModalModule
   ],
-  providers: [],
+  providers: [
+    BsModalService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
